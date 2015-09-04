6278c8ba0f8681e499f3bc11c78933150362db0c
J. Peter M. Schuler <jpmschuler@users.noreply.github.com>
2015-09-04 12:29:09 +0200

    tap cleanup

--- pdf2latex.rb
+++ pdf2latex.rb
@@ -1,9 +1,9 @@
 class Pdf2latex < Formula
   desc "A simple script which cuts a multi-page PDF(s) in single pages and crops white margin."
   homepage "https://github.com/jpmschuler/homebrew-tap"
-  url "https://github.com/jpmschuler/homebrew-tap/blob/master/downloads/pdf2latex-1.0.1.tar.gz?raw=true"
-  version "1.0.2"
-  sha256 "2ff5df40ac0e6148b6471c21ad75e2c139ebf7a6880da459d44a5bdbdf354eb0"
+  url "https://www.uni-due.de/~sw3081/homebrew/pdf2latex/pdf2latex-0.5.0.tar.gz"
+  version "0.5.0"
+  sha256 "05be10e0a8679a2a21298246aa4df57a845fd8e4b06e1bc1076338fc98f5b19a"
 
   depends_on "ghostscript"
   depends_on "qpdf"
