23a72635005f8e9c3985b7da92b2bc56b24943f5
J. Peter M. Schuler <jpmschuler@users.noreply.github.com>
2015-09-04 12:13:25 +0200

    next test

--- pdf2latex.rb
+++ pdf2latex.rb
@@ -2,7 +2,7 @@ class Pdf2latex < Formula
   desc "A simple script which cuts a multi-page PDF(s) in single pages and crops white margin."
   homepage "https://github.com/jpmschuler/homebrew-tap"
   url "https://github.com/jpmschuler/homebrew-tap/blob/master/downloads/pdf2latex-1.0.1.tar.gz?raw=true"
-  version "1.0.1"
+  version "1.0.2"
   sha256 "2ff5df40ac0e6148b6471c21ad75e2c139ebf7a6880da459d44a5bdbdf354eb0"
 
   depends_on "ghostscript"
