170ae43f5413bea27c8b9bb2138202d42677c704
J. Peter M. Schuler <jpmschuler@users.noreply.github.com>
2015-09-04 12:12:09 +0200

    next test

--- pdf2latex.rb
+++ pdf2latex.rb
@@ -5,9 +5,11 @@ class Pdf2latex < Formula
   version "1.0.1"
   sha256 "2ff5df40ac0e6148b6471c21ad75e2c139ebf7a6880da459d44a5bdbdf354eb0"
 
+  depends_on "ghostscript"
+  depends_on "qpdf"
 
   def install
-    bin.install "pdf2latex.sh"
+    bin.install "pdf2latex/pdf2latex.sh"
   end
 
 end
