53725124f3adb8823eab1afd84bc6b87b6ce71e3
J. Peter M. Schuler <jpmschuler@users.noreply.github.com>
2015-09-08 18:02:05 +0200

    correcting build scripts

--- ffmpeg-tools.rb
+++ ffmpeg-tools.rb
@@ -8,10 +8,10 @@ class FfmpegTools < Formula
   depends_on "ffmpeg" => %w{with-vpx with-faac with-vorbis with-libvorbis with-vpx with-vorbis with-theora with-libogg with-libvorbis with-gpl with-version3 with-nonfree with-postproc with-libaacplus with-libass with-libcelt with-libfaac with-libfdk-aac with-libfreetype with-libmp3lame with-libopencore-amrnb with-libopencore-amrwb with-libopenjpeg with-openssl with-libopus with-libschroedinger with-libspeex with-libtheora with-libvo-aacenc with-libvorbis with-libvpx with-libx264 with-libxvid}
 
   def install
-    bin.install "ffmpeg-tools/ffmpeg-tools-convert-to-html5video"
-    bin.install "ffmpeg-tools/ffmpeg-tools-convert-to-mp4-and-removenoise"
-    bin.install "ffmpeg-tools/ffmpeg-tools-create-videothumbs"
-    bin.install "ffmpeg-tools/ffmpeg-tools-remove-audionoise-from-mp4"
+    bin.install "ffmpeg-tools-convert-to-html5video"
+    bin.install "ffmpeg-tools-convert-to-mp4-and-removenoise"
+    bin.install "ffmpeg-tools-create-videothumbs"
+    bin.install "ffmpeg-tools-remove-audionoise-from-mp4"
   end
 
 end
