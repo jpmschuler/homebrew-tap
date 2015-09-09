dbe71108fef928d9b6eba3192bf2eeeec9b73d5e
J. Peter M. Schuler <jpmschuler@users.noreply.github.com>
2015-09-09 09:45:34 +0200

    correcting ffmpeg build options

--- ffmpeg-tools.rb
+++ ffmpeg-tools.rb
@@ -5,7 +5,7 @@ class FfmpegTools < Formula
   version "0.1.0"
   sha256 "10c48738befcc2e9a573f1b1bc1952e51af40263fa739d7044e0cab6892b95f1"
 
-  depends_on "ffmpeg" => %w{with-vpx with-faac with-vorbis with-libvorbis with-vpx with-vorbis with-theora with-libogg with-libvorbis with-gpl with-version3 with-nonfree with-postproc with-libaacplus with-libass with-libcelt with-libfaac with-libfdk-aac with-libfreetype with-libmp3lame with-libopencore-amrnb with-libopencore-amrwb with-libopenjpeg with-openssl with-libopus with-libschroedinger with-libspeex with-libtheora with-libvo-aacenc with-libvorbis with-libvpx with-libx264 with-libxvid}
+  depends_on "ffmpeg" => %w{with-faac with-theora with-libass with-openssl with-libvorbis with-libvpx}
 
   def install
     bin.install "ffmpeg-tools-convert-to-html5video"
